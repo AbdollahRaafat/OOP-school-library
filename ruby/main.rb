eef68ed38b7527ef7c22b52bf566e68b501cef85
abdollahraafat <abdollah.raafat.daif@gmail.com>
2022-10-01 20:58:58 +0200

    solve linter error

--- ruby/main.rb
+++ ruby/main.rb
@@ -6,18 +6,13 @@ require_relative 'options/get_rentals'
 
 def main
   library = {
-    books: [],
-    people: [],
-    rentals: []
+    books: [], people: [], rentals: []
   }
 
   options = {
-    '1': 'List all books',
-    '2': 'List all people',
-    '3': 'Create a person',
-    '4': 'Create a book',
-    '5': 'Create a rental',
-    '6': 'List all rentals for a given person id',
+    '1': 'List all books', '2': 'List all people',
+    '3': 'Create a person', '4': 'Create a book',
+    '5': 'Create a rental', '6': 'List all rentals for a given person id',
     '7': 'Exit'
   }
 
